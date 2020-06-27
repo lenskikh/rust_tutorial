e04076d6f20192ba50e14afc1d939f0d4951084f
Mikhail Lenskikh <lenskikh@gmail.com>
2020-06-27 00:10:44 -0400

    minor

--- create_file/target/debug/deps/opfl-5330f1a2782e8f25.d
+++
@@ -1,5 +0,0 @@
-/Users/happybird/Desktop/Dev/rust/opfl/target/debug/deps/opfl-5330f1a2782e8f25: src/main.rs
-
-/Users/happybird/Desktop/Dev/rust/opfl/target/debug/deps/opfl-5330f1a2782e8f25.d: src/main.rs
-
-src/main.rs:
--- create_file/target/debug/opfl.d
+++
@@ -1,1 +0,0 @@
-/Users/happybird/Desktop/Dev/rust/create_file/target/debug/opfl: /Users/happybird/Desktop/Dev/rust/create_file/src/main.rs
