1064561fb7214b1d3c7f25ba7828c1e127feb9f1
Mikhail Lenskikh <lenskikh@gmail.com>
2020-06-30 02:01:55 -0400

    final game

--- random_numbers/src/main.rs
+++ random_numbers/src/main.rs
@@ -4,17 +4,16 @@ use rand::Rng;
 
 fn main() {
 
-// --snip--
-
 let mut guess = String::new();
+
+println!("Guess the number!");
+let secret_number = rand::thread_rng().gen_range(1, 101);
 
 io::stdin().read_line(&mut guess)
     .expect("Failed to read line");
 
 let guess: u32 = guess.trim().parse()
     .expect("Please type a number!");
-
-println!("You guessed: {}", guess);
 
 match guess.cmp(&secret_number) {
     Ordering::Less => println!("Too small!"),
