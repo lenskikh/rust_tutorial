2613878fe4dd12dba4964b289ec0a8ddcd594802
Mikhail Lenskikh <lenskikh@gmail.com>
2020-06-30 00:00:07 -0400

    minor

--- random_numbers/src/main.rs
+++ random_numbers/src/main.rs
@@ -1,19 +1,24 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+// --snip--
 
-    println!("The secret number is: {}", secret_number);
+let mut guess = String::new();
 
-    println!("Please input your guess.");
+io::stdin().read_line(&mut guess)
+    .expect("Failed to read line");
 
-    let mut guess = String::new();
+let guess: u32 = guess.trim().parse()
+    .expect("Please type a number!");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
+match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal => println!("You win!"),
+}
 }
